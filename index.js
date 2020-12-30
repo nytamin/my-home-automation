57a2325c82eea217b36b85db6dbca9f3fbd281f1
Johan Nyman <johan@nytamin.se>
2020-12-30 15:03:09 +0100

    upd, move settings example file

--- index.js
+++ index.js
@@ -1,43 +1,58 @@
-var Cron = require('cron')
-const { updateElecticalPrice } = require('./electricalPrice')
-const { updateStore } = require('./dataStore')
-const { updateWeather } = require('./weather')
-const { updateSensiboTemperature } = require('./sensibo')
-const { optimizeHeater } = require('./optimizer')
-const { executeHeater } = require('./execute')
-const { updateEaseeStatus } = require('./easee')
-const { updateShellyTemperature } = require('./shelly')
+const fs = require('fs')
 
 console.log('===========================================================')
 console.log('My Home Automation')
 console.log('===========================================================')
 
 
-async function hourlyJob () {
-    console.log('*** Hourly job ***')
+if (!fs.existsSync('./settings.js')) {
+    fs.copyFileSync('./settings_example.js', './settings.js')
 
-    // Update electrical prices:
-    await updateElecticalPrice()
+    console.log('Please fill in settings.js file!')
+    process.exit(-1)
 
-    await updateWeather()
+} else {
 
-    await updateSensiboTemperature()
 
-    // await updateEaseeStatus() // todo: implement
+    const Cron = require('cron')
+    const { updateElecticalPrice } = require('./electricalPrice')
+    const { updateStore } = require('./dataStore')
+    const { updateWeather } = require('./weather')
+    const { updateSensiboTemperature } = require('./sensibo')
+    const { optimizeHeater } = require('./optimizer')
+    const { executeHeater } = require('./execute')
+    const { updateEaseeStatus } = require('./easee')
+    const { updateShellyTemperature } = require('./shelly')
 
-    // TODO: updateTemperaturemeasure
 
-    await updateShellyTemperature()
 
-    const simulation = optimizeHeater()
+    async function hourlyJob () {
+        console.log('*** Hourly job ***')
 
-    await executeHeater(simulation)
+        // Update electrical prices:
+        await updateElecticalPrice()
 
-    // Finally:
-    updateStore()
+        await updateWeather()
+
+        await updateSensiboTemperature()
+
+        // await updateEaseeStatus() // todo: implement
+
+        // TODO: updateTemperaturemeasure
+
+        await updateShellyTemperature()
+
+        const simulation = optimizeHeater()
+
+        await executeHeater(simulation)
+
+        // Finally:
+        updateStore()
+    }
+
+    // Set up cron jobs:
+    var hourly = new Cron.CronJob('1 * * * *', () => {
+        hourlyJob().catch(console.error)
+    }, undefined, true, 'Europe/Stockholm', undefined, true)
 }
 
-// Set up cron jobs:
-var hourly = new Cron.CronJob('1 * * * *', () => {
-    hourlyJob().catch(console.error)
-}, undefined, true, 'Europe/Stockholm', undefined, true)
